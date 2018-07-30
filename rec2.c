7125bfbc58c9b692becfbead36277a3474df200b
Rajat Goyal <31059368+rajatgoyall@users.noreply.github.com>
2018-07-29 22:02:05 -0700

    basic recursion

---
+++ rec2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+void printFun(int test)
+{
+    if (test < 1)
+        return;
+    else
+    {
+        cout << test << " ";
+        printFun(test-1);    // statement 2
+        cout << test << " ";
+        return;
+
+    }
+}
+ 
+int main()
+{
+    int test = 3;
+    printFun(test);
+    printFun(test-1);
+    printFun(test);
+}

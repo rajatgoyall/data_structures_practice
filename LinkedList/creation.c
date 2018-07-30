1becd01be1969f3286000270deaa19956aa8e9ba
Rajat Goyal <31059368+rajatgoyall@users.noreply.github.com>
2018-07-29 22:57:08 -0700

    basic ds implementation

---
+++ LinkedList/creation.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+struct Node 
+{
+	int data;
+	struct Node* next;
+};
+
+void push(struct Node** head_ref,int new_data)
+{
+	struct Node* new_node=(struct Node*)malloc(sizeof(struct Node));
+	new_node->data=new_data;
+	new_node->next=(*head_ref);
+	(*head_ref)=new_node;
+}
+
+void InsertAfter(struct Node* prev_node,int new_data)
+{
+	if(prev_node==NULL)
+	{
+		printf("the previous Node cannot be NULL");
+		return;
+	}	
+
+	struct Node* new_node=(struct Node*)malloc(sizeof(struct Node));
+	new_node->data=new_data;
+	new_node->next=prev_node->next;
+	prev_node->next=new_node;
+
+}
+
+void append(int new_data)
+{
+	struct Node* new_node=(struct Node*)malloc(sizeof(struct Node));
+	struct Node* temp=(*head_ref);
+	new_node->data=new_data;
+	new_node->next=NULL;
+	if(*head_ref==NULL)
+	{
+		*head_ref=new_node;
+		return;
+	}
+	
+	while(temp->next)
+	{
+		temp=temp->next;
+	}
+	temp->next=new_node;
+}
+
+void printList(struct Node *node)
+{
+  while (node != NULL)
+  {
+     printf(" %d ", node->data);
+     node = node->next;
+  }
+
+
+
+int main()
+{
+	struct Node* head=NULL;
+	push(&head,5);
+	append(3);
+	append(7);
+	InsertAfter(head->next,5);
+
+
+}
